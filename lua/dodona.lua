154d55c679d85cf86822da0aa39b622956abe61d
Xander <xandbi@gmail.com>
2022-03-26 13:27:29 +0100

    terminal

--- lua/dodona.lua
+++ lua/dodona.lua
@@ -1,19 +1,21 @@
 local fn = vim.fn
-local plugin_path = fn.stdpath("data") .. "/site/pack/packer/start/dodona.nvim"
+local command = "python " .. fn.stdpath("data") .. "/site/pack/packer/start/dodona.nvim" .. "/scripts/python/main.py "
 
 local M = {}
 
 function M.submit()
 	local file = fn.expand("%")
 	local options = "--command submit --path " .. file
-	vim.cmd("!python " .. plugin_path .. "/scripts/python/main.py " .. options)
+	vim.cmd("15split | terminal")
+	vim.cmd(':call jobsend(b:terminal_job_id, " ' .. command .. options .. '")')
 end
 
 function M.initActivities()
 	local series_id = fn.input("Series id: ")
 	local current_dir = fn.getcwd()
 	local options = "--command init --series " .. series_id .. " --path " .. current_dir
-	vim.cmd("!python " .. plugin_path .. "/scripts/python/main.py " .. options)
+	vim.cmd("15split | terminal")
+	vim.cmd(':call jobsend(b:terminal_job_id, " ' .. command .. options .. '")')
 end
 
 return M
