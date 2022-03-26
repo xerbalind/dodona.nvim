f6bab70c25dc36facd71c8303414d43db578c479
Xander <xandbi@gmail.com>
2022-03-26 13:10:21 +0100

    add submit functionality

--- lua/dodona.lua
+++ lua/dodona.lua
@@ -1,17 +1,18 @@
 local fn = vim.fn
+local plugin_path = fn.stdpath("data") .. "/site/pack/packer/start/dodona.nvim"
 
 local M = {}
 
 function M.submit()
-	local file = fn.expand("%:p")
-	print(file)
+	local file = fn.expand("%")
+	local options = "--command submit --path " .. file
+	vim.cmd("!python " .. plugin_path .. "/scripts/python/main.py " .. options)
 end
 
 function M.initActivities()
-	local course_id = fn.input("Course id: ")
-	local plugin_path = fn.stdpath("data") .. "/site/pack/packer/start/dodona.nvim"
+	local series_id = fn.input("Series id: ")
 	local current_dir = fn.getcwd()
-	local options = "--command init --series " .. course_id .. " --path " .. current_dir
+	local options = "--command init --series " .. series_id .. " --path " .. current_dir
 	vim.cmd("!python " .. plugin_path .. "/scripts/python/main.py " .. options)
 end
 
